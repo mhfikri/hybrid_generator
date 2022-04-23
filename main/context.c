025e8e33b8321ab2da5bdb59be852cb1a6af7be8
Fikri <mhfikri@outlook.com>
2022-04-22 18:03:23 -0700

    Add context.c

---
+++ main/context.c
@@ -0,0 +1,49 @@
+#include "freertos/FreeRTOS.h"
+#include "freertos/event_groups.h"
+
+#include "esp_err.h"
+
+#include "context.h"
+
+#define context_set_flags(c, v, f) do {              \
+      if (v) {                                       \
+        xEventGroupSetBits((c)->event_group, (f));   \
+      } else {                                       \
+        xEventGroupClearBits((c)->event_group, (f)); \
+      }                                              \
+    } while (0)
+
+static const char *TAG = "config";
+
+context_t *context_create(void)
+{
+    context_t *context = calloc(1, sizeof(context_t));
+
+    context->event_group = xEventGroupCreate();
+
+    return context;
+}
+
+esp_err_t context_set_network_connected(context_t *context, bool connected)
+{
+    context_set_flags(context, connected, CONTEXT_EVENT_NETWORK);
+    return ESP_OK;
+}
+
+esp_err_t context_set_network_error(context_t *context, bool error)
+{
+    context_set_flags(context, error, CONTEXT_EVENT_NETWORK_ERROR);
+    return ESP_OK;
+}
+
+esp_err_t context_set_network_provisioned(context_t *context)
+{
+    xEventGroupSetBits(context->event_group, CONTEXT_EVENT_NETWORK_PROVISIONED);
+    return ESP_OK;
+}
+
+esp_err_t context_set_time_updated(context_t *context)
+{
+    xEventGroupSetBits(context->event_group, CONTEXT_EVENT_TIME);
+    return ESP_OK;
+}
---
+++ main/context.h
@@ -0,0 +1,36 @@
+#ifndef HYBRID_GENERATOR_CONTEXT_H
+#define HYBRID_GENERATOR_CONTEXT_H
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/event_groups.h"
+
+#include "esp_bit_defs.h"
+
+typedef enum {
+    CONTEXT_EVENT_NETWORK = BIT0,
+    CONTEXT_EVENT_NETWORK_PROVISIONED = BIT1,
+    CONTEXT_EVENT_NETWORK_ERROR = BIT2,
+    CONTEXT_EVENT_TIME = BIT3,
+} context_event_t;
+
+typedef struct {
+    EventGroupHandle_t event_group;
+    struct {
+        const char *device_id;
+        const char *ssid;
+        const char *password;
+        const char *firmware_version;
+    } config;
+} context_t;
+
+context_t *context_create(void);
+
+esp_err_t context_set_network_connected(context_t *context, bool connected);
+
+esp_err_t context_set_network_error(context_t *context, bool error);
+
+esp_err_t context_set_network_provisioned(context_t *context);
+
+esp_err_t context_set_time_updated(context_t *context);
+
+#endif /* HYBRID_GENERATOR_CONTEXT_H */
